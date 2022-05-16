ca3152d3b35c5e967eae7e0047e0ca5db09acd6a
Adil Siddiqui <adil.siddiqui@appyhigh.com>
2022-05-16 11:56:55 +0530

    first commit

---
+++ src/databases/index.ts
@@ -0,0 +1,11 @@
+import { MONGO_URI } from '@config';
+
+export const dbConnection = {
+  url: MONGO_URI,
+  options: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+};
